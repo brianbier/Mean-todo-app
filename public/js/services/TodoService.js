5018be8c65d508c0e3fdb4c43037898fa8041a00
Brian Bier <briansbier@gmail.com>
2016-06-30 11:56:28 -0400

    remove the app variable and created different modules for each controller and service

--- public/js/services/TodoService.js
+++ public/js/services/TodoService.js
@@ -1,32 +1,14 @@
-app.factory('TodoService',['$http',function($http){
-  return
-  get: function(){
-    return $http.get('api/todos')
-    .success(function(data){
-      return data
-    })
-    .error(function(error){
-      return error
-    });
-  }
-
-  create: function(TodoData){
-    return $http.post('api/todos',TodoData)
-    .success(function(data){
-      return data
-    })
-    .error(function(error){
-      return error
-    });
-  }
-
-  delete: function(todo_id){
-    return $http.post('api/todos/'+ todo_id)
-    .success(function(data){
-      return data
-    })
-    .error(function(error){
-      return error
-    });
+angular.module('todoService', [])
+.factory('TodoService',['$http',function($http){
+  return {
+    get: function(){
+      return $http.get('api/todos');
+    },
+    create: function(TodoData){
+      return $http.post('api/todos',TodoData);
+    },
+    delete: function(todo_id){
+      return $http.delete('api/todos/'+ todo_id);
+    }
   }
 }]);
