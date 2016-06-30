4944ef30573aaedab1dc364806e11bc5d7b88044
Brian Bier <briansbier@gmail.com>
2016-06-30 11:54:29 -0400

    Update the model to work better with the delete function

--- app/models/Todo.js
+++ app/models/Todo.js
@@ -2,11 +2,8 @@ var mongoose = require('mongoose');
 
 //Creating the schema
 var TodoSchema = mongoose.Schema(
-{
-  text: {type: String, default: ''}
-},
-{
-  timestamps: true
-});
+{text: {type: String, default: ''}},
+{done: {type: Boolean}},
+{timestamps: true});
 
 module.exports = mongoose.model('Todo',TodoSchema);
